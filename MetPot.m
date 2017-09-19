c612aa1cef7679ac1ba22a5abcb119704aa8fda2
emirojaseng <emirojaseng@gmail.com>
2017-09-19 08:25:19 -0500

    Update MetPot.m

--- MetPot.m
+++ MetPot.m
@@ -1,16 +1,19 @@
-function [vap,vep] = MetPot(A, q0, tol)
-%M�todo de Potencia aproxima el mayor vector propio y valor propio de A
-%usando el m�todo de potencia, tomando un q0 inicial, y una tolerancia tol
+function [sigmai, qi] = metodo_potencia(A, q0,k, tol)
+%Método de Potencia aproxima el mayor vector propio y valor propio de A
+%usando el método de potencia, tomando un q0 inicial, numero máximo 
+% de iteraciones k y una tolerancia tol
+
+if(size(q0,1)<size(q0,2)) %Garantiza que el vector q0 sea vertical
 q0=q0';
-q1=(A*q0)/elem_mayor(q0);
-cont=0;
-while(norm(q0-q1)/norm(q1)>tol)
- cont=cont+1;
- q0=q1;
- q1=(A*q0)/elem_mayor(q0);
-end  
-vap=elem_mayor(q1)
-vep=q1
-cont
 end
 
+q1=(A*q0)/elem_mayor(q0); %primera iteracion. q1 es el "nuevo" vector
+cont=0; %contador que cuenta el numero de iteraciones
+while(norm(q0-q1)/norm(q1)>tol && cont<k)
+ cont=cont+1;
+ q0=q1;
+ q1=(A*q0)/elem_mayor(q0); %Éste es el proceso importante
+end  
+sigmai=elem_mayor(q1);
+qi=q1;
+end
