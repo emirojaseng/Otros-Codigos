53bc1f875f7188469476c577e18cdb2c3e6fd0a7
emirojaseng <emirojaseng@gmail.com>
2017-09-25 14:46:59 -0500

    Update metodo_potencia_inv.m

--- metodo_potencia_inv.m
+++ metodo_potencia_inv.m
@@ -1,17 +1,20 @@
-function [vap,vep] = MetPotInv(A, q0, tol)
-%Método de Potencia aproxima el menor vector propio y valor propio de A
-%usando el método de potencia inversa, tomando un q0 inicial, y una tolerancia tol
+function [sigmai, qi] = metodo_potencia_inv(A, q0, rho, k, tol)
+%MÃ©todo de Potencia aproxima el menor vector propio y valor propio de A
+%usando el mÃ©todo de potencia inversa, tomando un q0 inicial, y una tolerancia tol
+
+if(size(q0,1)<size(q0,2)) %Garantiza que el vector q0 sea vertical
 q0=q0';
-q1=A\q0;
+end
+
+q1=(A-rho*eye(size(A,1)))\q0; %primera iteraciÃ³n
 q1=q1/elem_mayor(q0);
 cont=0;
-while(norm(q0-q1)/norm(q1)>tol)
+while(norm(q0-q1)/norm(q1)>tol && cont<k)
  cont=cont+1;
  q0=q1;
- q1=A\q0;
+ q1=(A-rho*eye(size(A,1)))\q0; %proceso importante
 q1=q1/elem_mayor(q0);
 end  
-vap=elem_mayor(q1)
-vep=q1
-cont
+sigmai=1/elem_mayor(q1)+rho; %despeja para obtener el vap de A
+qi=q1;
 end
