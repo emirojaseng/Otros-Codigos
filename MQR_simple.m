d0758a625028d953796657e0eaae26ac97d1584e
emirojaseng <emirojaseng@gmail.com>
2017-09-25 15:55:02 -0500

    Update MQR_simple.m

--- MQR_simple.m
+++ MQR_simple.m
@@ -1,13 +1,11 @@
 function [lambdas] = MQR_simple(A, k, tol)
 %MQR_simple Aplica el metodo QR simple, sin ninguna mejora rapidez
-A0=A;
 cont=0;
-while(norm(diag(A0,-1))>tol && cont<k)
+while(norm(diag(A,-1))>tol && cont<k) %Criterio para detenerlo
 cont=cont+1;
-[Q, R]=qr(A0);
-A1=R*Q;
-A0=A1;
+[Q, R]=qr(A); %factoriza en QR
+A=R*Q; %A_{i+1}=R_iQ_i
 end
-lambdas=diag(A0);
+lambdas=diag(A);
 end
 
