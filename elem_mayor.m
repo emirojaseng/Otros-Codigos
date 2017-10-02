28a755535347bca56e9bbe65787a0fe4a266578e
emirojaseng <emirojaseng@gmail.com>
2017-10-02 14:53:54 -0500

    Update elem_mayor.m

--- elem_mayor.m
+++ elem_mayor.m
@@ -1,10 +1,13 @@
 function mayor= elem_mayor(v)
 %elem_mayor Regresa el elemento cuyo valor absoluto sea mayor
 mayor=0;
-for i= 1:size(v,2)
+if(size(v,1)<size(v,2)) %Garantiza que el vector q0 sea vertical
+v=v';
+end
+
+for i= 1:size(v,1)
     if(abs(norm(v(i)))>abs(mayor))
         mayor=v(i);
     end
 end
 end
-
